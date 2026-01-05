0735967d71276271231526fa2c1457f75f0e2b12
Christopher M. Pierce <contact@chris-pierce.com>
2026-01-05 18:17:05 -0500

    setting up script for valgrind testing

---
+++ leak-check.sh
@@ -0,0 +1,3 @@
+# valgrind --leak-check=full --show-leak-kinds=all ./build/tests/test_read
+valgrind --leak-check=full --show-leak-kinds=all ./build/tests/test_write
+# valgrind --leak-check=full --show-leak-kinds=all ./build/tests/test_read_write
